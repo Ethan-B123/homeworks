276c9f2f3a83446acf5a2050c599d24d0fe5e9e6
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-10-05 19:45:44 -0700

    add map class

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -34,3 +34,43 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    @map.each do |kv|
+      if kv[0] == key
+        kv[1] = value
+        return true
+      end
+    end
+    @map << [key, value]
+    false
+  end
+
+  def lookup(key)
+    @map.each do |kv|
+      if kv[0] == key
+        return kv[1]
+      end
+    end
+    nil
+  end
+
+  def remove(key)
+    del_idx = nil
+    @map.each_with_index do |kv, idx|
+      if kv[0] == key
+        del_idx = idx
+      end
+    end
+    del_idx && @map.delete_at(del_idx)
+  end
+
+  def show
+    @map.dup
+  end
+end
