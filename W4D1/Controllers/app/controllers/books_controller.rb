408276cd4f1fe645151f7d1d318c649eb1f9b3e9
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-10-22 18:33:12 -0700

    Finish hw W4D1

--- W4D1/Controllers/app/controllers/books_controller.rb
+++ W4D1/Controllers/app/controllers/books_controller.rb
@@ -1,18 +1,25 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
     # your code here
+    render :new
   end
 
   def create
     # your code here
+    Book.new(book_params).save
+    redirect_to books_url
   end
 
   def destroy
     # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
