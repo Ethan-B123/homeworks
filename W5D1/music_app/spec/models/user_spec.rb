3bd6f259ee81aae5a8efe5146c7050ea7091ef04
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-10-30 08:56:17 -0700

    add tests to W5D1 hw

---
+++ W5D1/music_app/spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:user) do
+    FactoryGirl.build(:user,
+      email: "jonathan@fakesite.com",
+      password: "good_password")
+  end
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+end
