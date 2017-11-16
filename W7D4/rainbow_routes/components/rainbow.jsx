315943c31cf5b67a279ecfdf4c5aaed837fbec24
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-11-15 19:58:37 -0800

    add HW for W7D4

---
+++ W7D4/rainbow_routes/components/rainbow.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Green from './green';
+import Blue from './blue';
+import Violet from './violet';
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+        <NavLink exact to='/red' >red</NavLink>
+        <NavLink exact to='/green' >green</NavLink>
+        <NavLink exact to='/blue' >blue</NavLink>
+        <NavLink exact to='/violet' >violet</NavLink>
+        <NavLink to='/blue/indigo' >indigo</NavLink>
+        <NavLink to='/red/orange' >orange</NavLink>
+        <NavLink to='/red/yellow' >yellow</NavLink>
+        {/* Your links should go here */}
+
+        <div id="rainbow">
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Rainbow;
